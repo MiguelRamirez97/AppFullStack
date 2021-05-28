b26905f1e9c0fc58c3e8de4bab5c2c7fc6a08997
MiguelRamirez97 <73442213+MiguelRamirez97@users.noreply.github.com>
2021-05-28 12:16:24 -0500

    primer directriz funcional

--- front/src/components/Question.js
+++ front/src/components/Question.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import {Modal,ModalBody,ModalFooter,FormGroup} from 'reactstrap';
 
-export const Question = ({ question, excerpt, onDelete }) => (
-  <article className={excerpt ? 'question-excerpt' : 'question'}>
-    <h2>{question.question}</h2>
-    <p>{question.category}  - <small>{question.type}</small></p>
-   
-    {onDelete && (
-      <button className="button right" onClick={() => onDelete(question.id)}>DELETE</button>
-    )}
-    {excerpt && (
-      <Link to={`/question/${question.id}`} className="button">
-        View Question
-      </Link>
-    )}
-  </article>
-)
+export const Question = ({ question, excerpt, onDelete }) => {
+
+  const [state,setState]=useState(false);
+
+  const openModal = () => {
+    setState(!state)
+  }
+
+  return (
+    <article className={excerpt ? 'question-excerpt' : 'question'}>
+      <h2>{question.question}</h2>
+      <p>{question.category}  - <small>{question.type}</small></p>
+      {onDelete && (
+        <button className="button right" onClick={openModal}>DELETE</button>
+      )}
+      {excerpt && (
+        <Link to={`/question/${question.id}`} className="button">
+          View Question
+        </Link>
+      )}
+      <Modal isOpen={state}>
+        <ModalBody>
+          <FormGroup>
+            <p>Estas seguro de eliminar?</p>
+          </FormGroup>
+        </ModalBody>
+        <ModalFooter>
+          <button color="primary"onClick={() => onDelete(question.id)}>si</button>
+          <button color="secondary"onClick={openModal}>no</button>
+        </ModalFooter>
+      </Modal>
+    </article>
+  )
+}
