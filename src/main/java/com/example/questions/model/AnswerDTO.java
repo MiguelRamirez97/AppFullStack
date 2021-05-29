988a0ba5e257d9089db8634f0a169ac3fa265e3c
MiguelRamirez97 <73442213+MiguelRamirez97@users.noreply.github.com>
2021-05-29 13:19:49 -0500

    se realizo heroku push

--- src/main/java/com/example/questions/model/AnswerDTO.java
+++ src/main/java/com/example/questions/model/AnswerDTO.java
@@ -1,11 +1,13 @@
 package com.example.questions.model;
 
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import java.util.Objects;
 import java.util.Optional;
 
 public class AnswerDTO {
+    @NotBlank
     private String id;
     @NotBlank
     private String userId;
@@ -21,12 +23,7 @@ public class AnswerDTO {
 
     }
 
-//    public AnswerDTO(@NotBlank String questionId, @NotBlank String userId, @NotBlank String answer) {
-//        this.userId = userId;
-//        this.questionId = questionId;
-//        this.answer = answer;
-//    }
-    public AnswerDTO(String id,@NotBlank String questionId, @NotBlank String userId, @NotBlank String answer) {
+    public AnswerDTO(@NotBlank String id, @NotBlank String questionId, @NotBlank String userId, @NotBlank String answer) {
         this.id = id;
         this.userId = userId;
         this.questionId = questionId;
